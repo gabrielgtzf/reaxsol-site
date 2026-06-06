04f80f74856baf9ebe49c1c8a3f475b1f1c5317d
Gabriel Gutierrez Fuentes <g.gutierrez.f@forseti.org.mx>
2026-06-06 10:33:32 -0600

    Version Alfa de REAXSOL Site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://reaxsol.com", // cambiar por su dominio final
+  trailingSlash: "never",
+  build: {
+    assets: "_assets",
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        css: { charset: false },
+      },
+    },
+  },
+});
