0465375c3f5b1b7752587b32499091532c077b41
Gabriel Gutierrez Fuentes <g.gutierrez.f@forseti.org.mx>
2026-06-26 10:47:30 -0600

    Configura adaptador de Netlify (sin output hybrid)

---
+++ .netlify/v1/functions/ssr/.netlify/build/entry.mjs
@@ -0,0 +1,1 @@
+export { h as createHandler } from './chunks/ssr-function_BqRo7qfs.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://reaxsol.com", // cambiar por su dominio final
+  site: "https://reaxsol.com", // Cambia por tu dominio final
   trailingSlash: "never",
+  adapter: netlify(), // El adaptador se encarga del modo server
   build: {
     assets: "_assets",
   },
